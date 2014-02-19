656089ca0d12f2ecc32eebea996ba96eb18564c9
Silas Sewell <silas@sewell.org>
2014-02-18 23:10:26 -0800

    Add getCategories and stub out basics

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Shutterstock API
 
-Coming soon...
+``` javascript
+var shutterstock = require('shutterstock');
+
+var api = shutterstock({
+  user: 'api-user',
+  key: 'api-key',
+  version: '1',
+});
+
+api.getCategories(function(err, res, body) {
+  if (err) throw err;
+  console.log(body);
+});
+```
---
+++ test/v1.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var shutterstock = require('../lib');
+
+describe('v1', function() {
+  it('should require version', function(done) {
+    (function() {
+      shutterstock({
+        key: 'api-key',
+        user: 'api-user',
+      });
+    }).should.throw(/^Invalid options/);
+
+    done();
+  });
+
+  it('should require auth', function(done) {
+    (function() {
+      shutterstock({
+        version: '1',
+      });
+    }).should.throw(/^Invalid v1 options\n/);
+
+    done();
+  });
+
+  it('should initalize', function(done) {
+    var api = shutterstock({
+      key: 'api-key',
+      user: 'api-user',
+      version: '1',
+    });
+
+    api.version.should.eql('1');
+
+    done();
+  });
+});
